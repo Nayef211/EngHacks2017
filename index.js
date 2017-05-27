04dbd3415c20e4767ccac7d11d7587c72fad377c
Tracy Chen <tw4chen@edu.uwaterloo.ca>
2017-05-27 14:20:01 -0400

    Working database files

--- index.js
+++ index.js
@@ -1,7 +1,48 @@
 var mainText = document.getElementById("mainText");
 var submitButton = document.getElementById("submitButton");
+var firebaseReference = firebase.database().ref();
+var headingReference=firebaseReference.child("Technology"); //necessary
+var count=0;
+var clickCount=0;
+var localData;
+
+//following can get the values of subcategories
+var ratingRef = firebase.database().ref("NecessaryCategory/");
+ratingRef.orderByValue().on("value", function(data) {
+	localData = data;
+});
+
+//start of being able to insert into database
 function submitClick(){
-	var firebaseReference = firebase.database().ref();
-	var messageText=mainText.value;
-	firebaseReference.push().set(messageText);
+	clickCount++;
+	if (clickCount>count){
+		refresh();
+		count++;
+	}
+}
+
+function refresh(){
+	headingReference.on('value', function(dataSnap){
+		let value;
+		var messageText=mainText.value; // searched keyword
+		console.log(messageText);
+
+		localData.forEach((data) => {
+			if (messageText === data.key) {
+				value = Number(data.val()) + 1;
+				console.log('value', value);
+				console.log('data.val', data.val());
+			}
+		});
+
+		console.log(value);
+		var updates = {};
+		updates['/' + messageText] = value || 1;
+		return ratingRef.update(updates);			
+	});
+}
+
+if (clickCount>count){
+	refresh();
+	count++;
 }
